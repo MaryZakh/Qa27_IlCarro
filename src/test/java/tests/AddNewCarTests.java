37f7cc05a2c0b36c5c78cc67ef0ccf8bb1b19711
Maria Z <z0559882272@gmail.com>
2024-10-01 20:51:54 +0300

    added new car success

---
+++ src/test/java/tests/AddNewCarTests.java
@@ -0,0 +1,33 @@
+package tests;
+
+import models.Car;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class AddNewCarTests extends TestBase{
+
+    @Test
+    public void addNewCarSuccess(){
+        int i = (int) ((System.currentTimeMillis()/1000)%3600);
+        Car car = Car.builder()
+                .location("Tel Aviv, Israel")
+                .manufacture("Mazda")
+                .model("M3")
+                .year("2022")
+                .fuel("Petrol")
+                .seats(4)
+                .carClass("C")
+                .carRegNumber("678-900"+i)
+                .price(50)
+                .about("Nice car")
+                .build();
+
+        app.getHelperCar().openCarForm();
+        app.getHelperCar().fillCarForm();
+        app.getHelperCar().submitCarForm();
+    }
+
+
+
+}
